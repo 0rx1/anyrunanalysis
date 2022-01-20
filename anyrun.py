71926740412df0b5e111418ac39285f3a40d8f54
0r0x1 <mrspam91@gmail.com>
2022-01-20 19:22:52 +0100

    Update anyrun.py

--- anyrun.py
+++ anyrun.py
@@ -4,7 +4,7 @@ import time
 
 File = input("Enter File name: ")
 headers = {
-    'Authorization': 'API-Key ', # Enter your API key here
+    'Authorization': 'API-Key DEMOAPIKEY', # Enter your API key here
 }
 
 files = {
